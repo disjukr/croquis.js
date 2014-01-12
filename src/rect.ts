de888add9f4ebeff7bf47b869b7e4fdcb2dbc581
JongChan Choi <disjukr@naver.com>
2014-01-13 07:13:22 +0900

    saschanaz zzangzzangman

--- src/rect.ts
+++ src/rect.ts
@@ -1,70 +1,46 @@
 module Croquis {
     export class Rect {
-        private _x: number;
-        private _y: number;
-        private _width: number;
-        private _height: number;
+        x: number;
+        y: number;
+        width: number;
+        height: number;
         constructor (x: number , y: number, width: number, height: number) {
-            this._x = x;
-            this._y = y;
-            this._width = width;
-            this._height = height;
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
         }
         clone(): Rect {
-            return new Rect(this._x, this._y, this._width, this._height);
+            return new Rect(this.x, this.y, this.width, this.height);
         }
         isEmpty(): boolean {
-            return this._width === 0 || this._height === 0;
-        }
-        get x(): number {
-            return this._x;
-        }
-        set x(value: number) {
-            this._x = value;
-        }
-        get y(): number {
-            return this._y;
-        }
-        set y(value: number) {
-            this._y = value;
-        }
-        get width(): number {
-            return this._width;
-        }
-        set width(value: number) {
-            this._width = value;
-        }
-        get height(): number {
-            return this._height;
-        }
-        set height(value: number) {
-            this._height = value;
+            return this.width === 0 || this.height === 0;
         }
         get left(): number {
-            return this._x;
+            return this.x;
         }
         set left(value: number) {
-            this._width += this._x - value;
-            this._x = value;
+            this.width += this.x - value;
+            this.x = value;
         }
         get right(): number {
-            return this._x + this._width;
+            return this.x + this.width;
         }
         set right(value: number) {
-            this._width = value - this._x;
+            this.width = value - this.x;
         }
         get top(): number {
-            return this._y;
+            return this.y;
         }
         set top(value: number) {
-            this._height += this._y - value;
-            this._y = value;
+            this.height += this.y - value;
+            this.y = value;
         }
         get bottom(): number {
-            return this._y + this._height;
+            return this.y + this.height;
         }
         set bottom(value: number) {
-            this._height = value - this._y;
+            this.height = value - this.y;
         }
     }
 }
